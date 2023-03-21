8aaffefc02f2f6a2aa2b1aa2d39e77e897865277
nithiya <nithiyasrisri3@gmail.com>
2023-03-21 12:08:33 +0530

    adding file

---
+++ practice /currency calc /js/main.js
@@ -0,0 +1,36 @@
+// let inr=parseInt(prompt("enter the number"));
+// //inr=parseInt(inr);
+// let usd;
+// usd=inr/80;
+//console.log("Rs :"+inr+"= $"+usd);
+// function DollerToInr(usd){
+// 	usd=parseInt(usd);
+// 	let inr=usd*8;
+// 	console.log("$ :"+usd+"= Rs"+inr);
+//
+// }
+// function inrToDoller(inr){
+// 	inr=parseInt(inr);
+// 	let usd=inr/80;
+// 	console.log("Rs :"+inr+"= $"+usd);
+//
+// }
+// function showWarning(){
+// 	console.log("enter correct conversion type");
+// }
+// let value=prompt("enter the number");
+// conversionType=prompt("enter D2I or I2D");
+//
+// if (conversionType=="D2I"){
+//
+// 	DollerToInr(value);
+// }
+// else if (conversionType=="I2D")
+// {
+//
+// 	inrToDoller(value);
+// }
+// else
+// {
+// 	showWarning();
+// }
---
+++ practice /loop/js/main.js
@@ -0,0 +1,15 @@
+ var table=parseInt(prompt("enter the number"));
+var upto=16;
+ var count=1;
+ while(count<=upto)
+ {
+ 	console.log(table * count)
+     count=count+1
+ }
+// var table=6
+// var upto=16;
+//
+// for (var count=1;count<upto;count++)
+// {
+// 	console.log(table*count)
+// }

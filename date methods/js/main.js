dcb0f1a7c8434cc2bbafd6067d9fa948300aa1b9
nithiya <nithiyasrisri3@gmail.com>
2023-03-21 11:58:44 +0530

    adding file

---
+++ date methods/js/main.js
@@ -0,0 +1,23 @@
+let a=parseInt(prompt("enter a value"))
+if( a==0)
+{
+	console.log(true)
+}
+else
+{
+	console.log(false)
+}
+// a=[2,3,4,5,6,7,8,9,10]
+// b=[]
+// for(i=0;i<a.length;i++)
+// {
+// 	b.push(a[i]);
+// 	console.log(b);
+//
+// }
+// a=[4,3,2]
+// b=" "
+// for(i=0;i<a.length;i++)
+// {
+// 	console.log(b=b+a[i]+"\n")
+// }

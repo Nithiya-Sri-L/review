499eee62c2499856edd0655a92a3d17bd0ecefb0
nithiya <nithiyasrisri3@gmail.com>
2023-03-21 12:49:04 +0530

    adding a file

---
+++ js_quiz/js/quiz.js
@@ -0,0 +1,7 @@
+var quiz={
+	question:"The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is",
+    options:[{a:15},{b:16},{c:18},{d:25}],
+	answer:{
+		type:'single',obtained:'b'
+	}
+}console.log(quiz)
